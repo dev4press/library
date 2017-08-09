064d753d8880162e8271d42fead9f0961c5a885a
Milan Petrovic <milan@gdragon.info>
2017-08-09 22:02:40 +0200

    Minor update.

--- classes/d4p.cache.php
+++ classes/d4p.cache.php
@@ -46,12 +46,6 @@ if (!class_exists('d4p_cache_core')) {
             return $group.'::'.$key;
         }
 
-        public function in($group, $key) {
-            global $wp_object_cache;
-
-            return isset($wp_object_cache->cache[$this->store][$this->_key($group, $key)]);
-        }
-
         public function add($group, $key, $data, $expire = 0) {
             return wp_cache_add($this->_key($group, $key), $data, $this->store, $expire);
         }
@@ -76,12 +70,31 @@ if (!class_exists('d4p_cache_core')) {
             return wp_cache_delete($this->_key($group, $key), $this->store);
         }
 
+        /** @global WP_Object_Cache $wp_object_cache */
+        public function in($group, $key) {
+            global $wp_object_cache;
+
+            return isset($wp_object_cache->cache[$this->store][$this->_key($group, $key)]);
+        }
+
+        /** @global WP_Object_Cache $wp_object_cache */
         public function clear() {
             global $wp_object_cache;
 
             if (isset($wp_object_cache->cache[$this->store])) {
                 unset($wp_object_cache->cache[$this->store]);
             }
+        }
+
+        /** @global WP_Object_Cache $wp_object_cache */
+        public function storage() {
+            global $wp_object_cache;
+            
+            if (isset($wp_object_cache->cache[$this->store])) {
+                return $wp_object_cache->cache[$this->store];
+            }
+
+            return array();
         }
     }
 }
