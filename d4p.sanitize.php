e206a54be5a3061f59b3306abc531574af90ccf6
Milan Petrovic <milan@gdragon.info>
2018-06-18 16:44:07 +0200

    File path sanitize function.

--- d4p.sanitize.php
+++ d4p.sanitize.php
@@ -2,7 +2,7 @@
 
 /*
 Name:    d4pLib_Sanitize
-Version: v2.3.1
+Version: v2.3.2
 Author:  Milan Petrovic
 Email:   support@dev4press.com
 Website: https://www.dev4press.com/
@@ -24,6 +24,24 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+if (!function_exists('d4p_sanitize_file_path')) {
+    function d4p_sanitize_file_path($filename) {
+	$filename_raw = $filename;
+
+        $special_chars = apply_filters('d4p_sanitize_file_path_chars', array(
+            "?", "[", "]", "/", "\\", "=", "<", ">", ":", ";", ",", "'", "\"", "&", "$", "#", "*", "(", ")", "|", "~", "`", "!", "{", "}", "%", "+", chr(0)
+            ), $filename_raw);
+
+	$filename = preg_replace("#\x{00a0}#siu", ' ', $filename);
+	$filename = str_replace($special_chars, '', $filename);
+	$filename = str_replace(array('%20', '+'), '-', $filename);
+	$filename = preg_replace('/[\r\n\t -]+/', '-', $filename);
+	$filename = trim($filename, '.-_' );
+
+        return apply_filters('d4p_sanitize_file_path', $filename, $filename_raw);
+    }
+}
 
 if (!function_exists('d4p_sanitize_key_expanded')) {
     function d4p_sanitize_key_expanded($key) {
