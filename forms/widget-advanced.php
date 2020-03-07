006ec037b3a20ef628920486f517a0ab5fa1a18e
Milan Petrovic <milan@gdragon.info>
2020-03-07 09:17:59 +0100

    Work in progress.

--- forms/widget-advanced.php
+++ forms/widget-advanced.php
@@ -1,11 +1,11 @@
 <?php
 
 $list_users_visibility = array(
-    'all' => __("Everyone"),
-    'users' => __("Logged in users"),
-    'visitors' => __("Not logged in visitors"),
-    'roles' => __("Users with specified roles"),
-    'caps' => __("Users with specified capabilities")
+    'all' => __("Everyone", "d4plib"),
+    'users' => __("Logged in users", "d4plib"),
+    'visitors' => __("Not logged in visitors", "d4plib"),
+    'roles' => __("Users with specified roles", "d4plib"),
+    'caps' => __("Users with specified capabilities", "d4plib")
 );
 
 ?>
@@ -30,7 +30,7 @@ $list_users_visibility = array(
                     <?php d4p_render_checkradios(d4p_get_wordpress_user_roles(), array('id' => $this->get_field_id('_roles'), 'class' => 'widefat', 'name' => $this->get_field_name('_roles'), 'selected' => $instance['_roles'])); ?>
 
                     <em>
-                        <?php _e("If no roles are selected, widget will assume that all roles are selected.", "gd-topic-polls"); ?>
+                        <?php _e("If no roles are selected, widget will assume that all roles are selected.", "d4plib"); ?>
                     </em>
                 </td>
             </tr>
@@ -64,7 +64,7 @@ $list_users_visibility = array(
                 <input class="widefat" id="<?php echo $this->get_field_id('_hook'); ?>" name="<?php echo $this->get_field_name('_hook'); ?>" type="text" value="<?php echo esc_attr($instance['_hook']); ?>" />
 
                 <em>
-                    <?php echo sprintf(__("Using this value, widget will run filter for the final visibility check. This filter will be %s, replace the %s with your custom filter name defined here,", "d4plib"), "<strong>'".$this->visibility_hook_format()."'</strong>", "<strong>'{filter_name}'</strong>"); ?>
+                    <?php echo sprintf(__("Using this value, widget will run filter for the final visibility check. This filter will be %s, replace the %s with your custom filter name defined here.", "d4plib"), "<strong>'".$this->visibility_hook_format()."'</strong>", "<strong>'{filter_name}'</strong>"); ?>
                 </em>
             </td>
         </tr>
--- forms/widget-extra.php
+++ forms/widget-extra.php
@@ -15,3 +15,19 @@
         </tr>
     </tbody>
 </table>
+
+<h4><?php _e("For Developers", "d4plib"); ?></h4>
+<table>
+    <tbody>
+        <tr>
+            <td class="cell-singular">
+                <label for="<?php echo $this->get_field_id('_devid'); ?>"><?php _e("Developer ID", "d4plib"); ?>:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('_devid'); ?>" name="<?php echo $this->get_field_name('_devid'); ?>" type="text" value="<?php echo esc_attr($instance['_devid']); ?>" />
+
+                <em>
+                    <?php _e("This is custom string (alphanumeric, underscore and minus signs allowed) that can be used to identify the widget in code, and it can be useful for developers customizing the website.", "d4plib"); ?>
+                </em>
+            </td>
+        </tr>
+    </tbody>
+</table>
