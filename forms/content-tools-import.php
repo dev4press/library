5031c8cb3ca7d2df3774eb47f8e36f5391162b9d
Milan Petrovic <milan@gdragon.info>
2020-05-24 11:57:23 +0200

    Few more updates.

--- forms/content-tools-import.php
+++ forms/content-tools-import.php
@@ -3,6 +3,7 @@
         <h3><?php _e("Important Information", "d4plib"); ?></h3>
         <div class="d4p-group-inner">
             <p><?php _e("With this tool you can import all plugin settings from the export file made using the Export tool. If you made changes to the export file, the import will fail.", "d4plib"); ?></p>
+            <p><?php _e("For import tools to work correctly, you must import a file made with the export tool running the same versions of the plugin for both import and export!", "d4plib"); ?></p>
         </div>
     </div>
 
