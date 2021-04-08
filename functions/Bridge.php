fd98931e7022aef8135c21df4b08efa7d668a165
Milan Petrovic <milan@gdragon.info>
2021-04-08 10:54:53 +0200

    One more fix.

--- functions/Bridge.php
+++ functions/Bridge.php
@@ -37,14 +37,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! function_exists( 'panel' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\panel' ) ) {
 	/** @return \Dev4Press\v35\Core\UI\Admin\Panel */
 	function panel() {
 		return Panel::instance();
 	}
 }
 
-if ( ! function_exists( 'local_image_to_media_library' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\local_image_to_media_library' ) ) {
 	function local_image_to_media_library( $path, $data = array(), $post_parent = 0, $args = array() ) {
 		$obj = new LocalImage( $path, $data, $args );
 
@@ -52,7 +52,7 @@ if ( ! function_exists( 'local_image_to_media_library' ) ) {
 	}
 }
 
-if ( ! function_exists( 'remote_image_to_media_library' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\remote_image_to_media_library' ) ) {
 	function remote_image_to_media_library( $url, $data = array(), $post_parent = 0, $args = array() ) {
 		$obj = new RemoteImage( $url, $data, $args );
 
@@ -60,7 +60,7 @@ if ( ! function_exists( 'remote_image_to_media_library' ) ) {
 	}
 }
 
-if ( ! function_exists( 'get_objects_sort' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\get_objects_sort' ) ) {
 	function get_objects_sort( $objects_array, $properties = array(), $uasort = false ) {
 		$_sort = new ObjectsSort( $objects_array, $properties, $uasort );
 
@@ -68,25 +68,25 @@ if ( ! function_exists( 'get_objects_sort' ) ) {
 	}
 }
 
-if ( ! function_exists( 'read_file' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\read_file' ) ) {
 	function read_file( $file_path, $part_size_mb = 2, $return_size = true ) {
 		return Download::instance( $file_path )->read_file( $part_size_mb, $return_size );
 	}
 }
 
-if ( ! function_exists( 'download_file_simple' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\download_file_simple' ) ) {
 	function download_file_simple( $file_path, $file_name = null, $gdr_readfile = true ) {
 		Download::instance( $file_path, $file_name )->simple( ! $gdr_readfile );
 	}
 }
 
-if ( ! function_exists( 'download_file_resume' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\download_file_resume' ) ) {
 	function download_file_resume( $file_path, $file_name = null ) {
 		Download::instance( $file_path, $file_name )->resume();
 	}
 }
 
-if ( ! function_exists( 'object_cache' ) ) {
+if ( ! function_exists( __NAMESPACE__ . '\object_cache' ) ) {
 	function object_cache() : Store {
 		return Store::instance();
 	}
