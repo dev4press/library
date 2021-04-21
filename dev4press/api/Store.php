ce0753f06dfffd8c9b166a81f062a5e369df4f3c
Milan Petrovic <milan@gdragon.info>
2021-04-21 17:12:05 +0200

    Few updates.

--- dev4press/api/Store.php
+++ dev4press/api/Store.php
@@ -32,15 +32,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Store {
 	private $_plugins = array(
-		/*"breadcrumbspress"              => array(
+		"archivespress"                    => array(
+			"code"        => "archivespress",
+			"name"        => "ArchivesPress",
+			"description" => "Display indexes based on the dates (years, months, and days archives), authors, and taxonomy terms for quick navigation and filtering of posts.",
+			"punchline"   => "Easy archives index based website navigation",
+			"color"       => "#46537A",
+			'free'        => true,
+			'pro'         => false
+		),
+		"breadcrumbspress"                 => array(
 			"code"        => "breadcrumbspress",
 			"name"        => "BreadcrumbsPress",
 			"description" => "Breadcrumbs based navigation, fully responsive and customizable, supporting post types, all types of archives, 404 pages, search results and third-party plugins.",
 			"punchline"   => "Improve your website navigation with Breadcrumbs",
-			"color"       => "#0CA991",
+			"color"       => "#EA0000",
 			'free'        => true,
 			'pro'         => true
-		),*/
+		),
 		"debugpress"                       => array(
 			"code"        => "debugpress",
 			"name"        => "DebugPress",
