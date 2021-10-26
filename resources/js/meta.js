db782e86458fabeaae3dbead9ac059b316273b32
Milan Petrovic <milan@gdragon.info>
2021-10-26 09:40:00 +0200

    Work in progress.

--- resources/js/meta.js
+++ resources/js/meta.js
@@ -6,24 +6,6 @@
 
     window.wp.dev4press.metabox = {
         init: function() {
-            $(document).on("click", ".d4plib-meta-box-wrapper .wp-tab-bar a", function(e) {
-                e.preventDefault();
-
-                var tab = $(this).attr("href").substr(1),
-                    wrap = $(this).closest(".d4plib-meta-box-wrapper");
-
-                $(this).closest("ul").find("li").removeClass("wp-tab-active");
-                $(this).parent().addClass("wp-tab-active");
-
-                wrap.find(".wp-tab-panel")
-                    .removeClass("tabs-panel-active")
-                    .addClass("tabs-panel-inactive");
-
-                wrap.find("#" + tab)
-                    .removeClass("tabs-panel-inactive")
-                    .addClass("tabs-panel-active");
-            });
-
             $(document).on("click", ".d4plib-meta-box-wrapper .d4p-check-uncheck a", function(e) {
                 e.preventDefault();
 
