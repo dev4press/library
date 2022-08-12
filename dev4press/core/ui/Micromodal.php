e8b7506843719e8eef490f1897373eae7242d44d
Milan Petrovic <milan@gdragon.info>
2022-08-12 19:29:16 +0200

    Work in progress.

--- dev4press/core/ui/Micromodal.php
+++ dev4press/core/ui/Micromodal.php
@@ -31,8 +31,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Micromodal {
-	protected $defaults = array(
-		'modal-class'           => 'dev4press',
+	protected $defaults_args = array(
+		'modal-class'          => 'dev4press',
 		'modal-name'           => '',
 		'modal-title'          => 'Modal Dialog',
 		'modal-close-label'    => 'Close the Modal',
@@ -40,7 +40,14 @@ class Micromodal {
 		'modal-button-primary' => 'OK',
 		'modal-button-close'   => 'Close'
 	);
+	protected $defaults_settings = array(
+		'show-action-button' => true,
+		'close-by-overlay'   => true,
+		'close-by-cross'     => true,
+		'close-by-button'    => true
+	);
 	protected $args = array();
+	protected $settings = array();
 
 	public function __construct() {
 
@@ -56,8 +63,9 @@ class Micromodal {
 		return $instance;
 	}
 
-	public function init( array $args ) {
-		$this->args = wp_parse_args( $args, $this->defaults );
+	public function init( array $args, array $settings = array() ) {
+		$this->args     = wp_parse_args( $args, $this->defaults_args );
+		$this->settings = wp_parse_args( $settings, $this->defaults_settings );
 	}
 
 	public function dialog() : string {
@@ -96,14 +104,14 @@ class Micromodal {
 
 	protected function _open() : string {
 		return '<div data-modal="{{modal-name}}" class="micromodal-dialog-wrapper {{modal-class}}-modal {{modal-class}}-modal-slide" id="{{modal-class}}-modal-{{modal-name}}" aria-hidden="true">
-    <div class="{{modal-class}}-modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="{{modal-class}}-modal__overlay" tabindex="-1"' . ( $this->settings['close-by-overlay'] ? ' data-micromodal-close' : '' ) . '>
         <div class="{{modal-class}}-modal__container" role="dialog" aria-modal="true" aria-labelledby="{{modal-class}}-modal-{{modal-name}}-title">';
 	}
 
 	protected function _header() : string {
 		return '<header class="{{modal-class}}-modal__header">
                 <h2 class="{{modal-class}}-modal__title" id="{{modal-class}}-modal-{{modal-name}}-title">{{modal-title}}</h2>
-                <button type="button" class="{{modal-class}}-modal__close" aria-label="{{modal-close-label}}" data-micromodal-close></button>
+                ' . ( $this->settings['close-by-cross'] ? '<button type="button" class="{{modal-class}}-modal__close" aria-label="{{modal-close-label}}" data-micromodal-close></button>' : '' ) . '
             </header>';
 	}
 
@@ -115,8 +123,8 @@ class Micromodal {
 
 	protected function _footer() : string {
 		return '<footer class="{{modal-class}}-modal__footer">
-                <button type="button" id="{{modal-class}}-modal-{{modal-name}}-submit" class="{{modal-class}}-modal__btn {{modal-class}}-modal__btn-primary">{{modal-button-primary}}</button>
-                <button type="button" class="{{modal-class}}-modal__btn" data-micromodal-close>{{modal-button-close}}</button>
+                ' . ( $this->settings['show-action-button'] ? '<button type="button" id="{{modal-class}}-modal-{{modal-name}}-submit" class="{{modal-class}}-modal__btn {{modal-class}}-modal__btn-primary">{{modal-button-primary}}</button>' : '' ) . '
+                ' . ( $this->settings['close-by-button'] ? '<button type="button" class="{{modal-class}}-modal__btn" data-micromodal-close>{{modal-button-close}}</button>' : '' ) . '
             </footer>';
 	}
 
