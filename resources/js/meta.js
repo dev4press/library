f1e1fdd07b4411bf1b0bc94fe952f6cfd16e864e
Milan Petrovic <milan@gdragon.info>
2023-04-13 11:35:01 +0200

    Work in progress.

--- resources/js/meta.js
+++ resources/js/meta.js
@@ -1,22 +1,46 @@
 /*jslint regexp: true, nomen: true, undef: true, sloppy: true, eqeq: true, vars: true, white: true, plusplus: true, maxerr: 50, indent: 4 */
 
-;(function($, window, document, undefined) {
+;(function ($, window, document, undefined) {
     window.wp = window.wp || {};
     window.wp.dev4press = window.wp.dev4press || {};
+    window.wp.dev4press.v41 = window.wp.dev4press.v41 || {};
 
-    window.wp.dev4press.metabox = {
-        init: function() {
-            $(document).on("click", ".d4plib-meta-box-wrapper .d4p-check-uncheck a", function(e) {
+    window.wp.dev4press.v41.metabox = {
+        library: 'v41',
+        init: function () {
+            const wrapper = ".d4plib-" + wp.dev4press.v41.metabox.library + "-meta-box-wrapper";
+
+            $(document).on("click", wrapper + " .d4p-check-uncheck a", function (e) {
                 e.preventDefault();
 
                 var checkall = $(this).attr("href").substring(1) === "checkall";
 
                 $(this).parent().parent().find("input[type=checkbox]").prop("checked", checkall);
             });
+
+            $(wrapper + " .wp-tab-bar button").click(function (e) {
+                e.preventDefault();
+
+                var name = $(this).data("tab"),
+                    tab = $(this).parent(),
+                    tabs = $(this).closest(".wp-tab-bar"),
+                    wrap = $(this).closest(wrapper);
+
+                tabs.find("li").removeClass("wp-tab-active");
+                tab.addClass("wp-tab-active");
+
+                wrap.find(".wp-tab-panel")
+                    .removeClass("tabs-panel-active")
+                    .addClass("tabs-panel-inactive");
+
+                wrap.find("#" + name)
+                    .removeClass("tabs-panel-inactive")
+                    .addClass("tabs-panel-active");
+            });
         }
     };
 
-    $(document).ready(function() {
-        wp.dev4press.metabox.init();
+    $(document).ready(function () {
+        wp.dev4press.v41.metabox.init();
     });
 })(jQuery, window, document);
