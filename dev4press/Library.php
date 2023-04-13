1d9406a86bcd45040e9691c78dbb17e734259457
Milan Petrovic <milan@gdragon.info>
2023-04-13 10:46:30 +0200

    Work in progress.

--- dev4press/Library.php
+++ dev4press/Library.php
@@ -34,11 +34,11 @@ class Library {
 	/**
 	 * @var string
 	 */
-	private $_version = '4.0.1';
+	private $_version = '4.1';
 	/**
 	 * @var string
 	 */
-	private $_build = '4001';
+	private $_build = '4100';
 	/**
 	 * @var string
 	 */
