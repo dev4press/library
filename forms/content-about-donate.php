b99662d3d1f44932b50e227dbb1fdbfb70256fd7
Milan Petrovic <milan@gdragon.info>
2023-08-01 16:20:34 +0200

    Work in progress.

--- forms/content-about-donate.php
+++ forms/content-about-donate.php
@@ -14,7 +14,7 @@ use function Dev4Press\v43\Functions\panel;
         </div>
         <div class="core-col-sm-12 core-col-md-6">
             <h3><?php esc_html_e( "Development repository", "d4plib" ); ?></h3>
-            <p><?php echo esc_html( sprintf( __( "%s has a GitHub repository where you can contribute, or submit feature requests and suggestions.", "d4plib" ), panel()->a()->settings()->i()->name() ) ); ?></p>
+            <p><?php echo esc_html( sprintf( __( "%s has a GitHub repository where you can contribute to development, or submit feature requests and suggestions.", "d4plib" ), panel()->a()->settings()->i()->name() ) ); ?></p>
             <a href="<?php echo esc_url( panel()->a()->settings()->i()->github_url ); ?>" target="_blank" rel="noopener">
                 <span class="github-repo-button"><i class="d4p-icon d4p-brand-github-alt d4p-icon-4x"></i><span>GitHub Repository</span></span>
             </a>
